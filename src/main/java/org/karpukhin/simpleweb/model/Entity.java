f3865f48f79a15eeea81bbeac73dbdc417865a18
Pavel Karpukhin <karpukhin.pavel@gmail.com>
2011-10-14 17:51:53 +0400

    Entity class was updated

--- src/main/java/org/karpukhin/simpleweb/model/Entity.java
+++ src/main/java/org/karpukhin/simpleweb/model/Entity.java
@@ -4,4 +4,14 @@ package org.karpukhin.simpleweb.model;
  * @author Pavel Karpukhin
  */
 public class Entity {
+    
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
